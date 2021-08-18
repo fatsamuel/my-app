49ed4eea65214b875dc1d64c8c9fe34038ec98c7
Miguel Jimenez <migijim@hotmail.com>
2021-08-18 11:37:07 +0200

    V2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Tweet from './Tweet';
 import'./App.css';
 
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="app">
       
-      <Tweet name="Hans" message="Hello Hans" likes="5"/>
-      <Tweet name="Jesus" message="Hello Jesus" likes="5"/>
-      <Tweet name="Michael" message="Hello Michael" likes="5"/>
+      
     </div>
   );
 }
