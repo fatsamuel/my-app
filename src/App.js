78bd43e87e1771c7854acd98492956a5967b3089
Miguel Jimenez <migijim@hotmail.com>
2021-08-18 11:54:35 +0200

    V2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Tweet from './Tweet';
-import'./App.css';
+
 
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+
   return (
     <div className="app">
-      
+      <botton onClick={increment}>Increment</botton>
+      <h1>{count}</h1>
       
     </div>
   );
